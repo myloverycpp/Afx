974dca35809ed24404c090becf8472541d77da1c
zly <zly@iMac.local>
2016-05-10 11:33:03 +0800

    应用程序基础框架

---
+++ Afx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Afx
+//
+//  Created by zly on 16/5/10.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Afx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Afx
+//
+//  Created by zly on 16/5/10.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
